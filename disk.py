a2244b39d7735c9c0381c85023da85ad799e1b95
dani-l21 <daniel.dli2027@gmail.com>
2025-05-07 22:21:17 -0700

    Update disk.py

--- disk.py
+++ disk.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+#define constants
+GRAVITY_ACCEL = 9.81
+
 
 def final_disk_speed(height: float, length: float, incline: float, mass: float, friction: float, radius: float) -> float:
     """
@@ -13,3 +16,18 @@ def final_disk_speed(height: float, length: float, incline: float, mass: float, 
     :param radius: the radius of the disk (meters)
     :return: the speed of the disk (m/s)
     """
+    
+    #Solution Algorithm:
+        #Assume rolling without slipping, friction does no work
+        #Solve using conservation of energy:
+            #E_initial = E_final
+            #mgh = 1/2 mv^2 + 1/2 Iw^2
+                #rotational inertia: I = 1/2MR^2 for disk
+                #for rolling without slipping: v = rw, w = v/r
+            #mgh = 1/2 mv^2 + 1/2 (1/2 mR^2) (v/R)^2
+            #mgh = 1/2mv^2 + 1/4 mv^2
+            #v = sqrt(4/3 gh)
+        #return speed
+
+    
+    return np.sqrt(4/3*GRAVITY_ACCEL*height)
